31cf5062fd434fa1bf35f319aba5a4ee8578b42e
彭畅 <1326278155@qq.com>
2025-09-11 16:48:11 +0800

    first component

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -1,5 +1,6 @@
 import { makeInstaller } from '@pc-element/utils';
 import components from './components';
+import '@pc-element/theme/index.css';
 const installer = makeInstaller(components);
 export * from '@pc-element/components';
 export default installer;
